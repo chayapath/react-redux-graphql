46e67fa9050fa694a7a4a59b243258a20484b327
chayapath <chayapath.t@gmail.com>
2021-07-27 21:34:10 +0700

    Init homePageSlice

--- src/app/containers/HomePage/homePageSlice.tsx
+++ src/app/containers/HomePage/homePageSlice.tsx
@@ -1,17 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  counter: 0,
+  animeList: []
 };
 const HomePageSlice = createSlice({
   name: "homePage",
   initialState,
-  reducers: {
-    incrementCounter: (state, action) => {
-      state.counter += 1;
-    },
-  },
+  reducers: {},
 });
 
-export const { incrementCounter } = HomePageSlice.actions;
+export const { } = HomePageSlice.actions;
 export default HomePageSlice.reducer;
