e3934f9f603b2a93f2bed5f25ee98dec9138c9b5
chayapath <chayapath.t@gmail.com>
2021-07-28 00:42:21 +0700

    Revert "Create selectors.ts"
    This reverts commit c8e1ed87ffdda7acf428c9c62cc82d6a4e31504c.

--- src/app/containers/HomePage/selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "reselect";
-import { IRootState } from "../../types";
-
-const selectHomePage = (state: IRootState) => state.homePage;
-
-export const makeSelectHomePage = createSelector(
-  selectHomePage,
-  (homePage) => homePage.animePage
-);
